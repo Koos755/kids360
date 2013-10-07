9fd2076e01069a7f2caea4ead8a0cdb0832fd3f2
Koos755 <justinusadriaanse@Justinuss-MacBook-Pro.local>
2013-10-07 13:16:12 +0200

    added seeds file
    As well as:
    password digest
    started styling parents index page
    worked a little an nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+parents = [{first_name: "Demarcus", last_name: "Bowser", phone_number: "9012894290", email: "bowser.dr@gmail.com", email_confirmed: true, address: "1381 S Willett St", city: "Memphis", state: "TN", zip: "38106", password: "MyPassword", password_confirmation: "MyPassword" },
+           {first_name: "Audrey", last_name: "Jones", phone_number: "9015684006", email: "audreypjones@gmail.com", email_confirmed: true, address: "1005 Idlewild", city: "Memphis", state: "TN", zip: "38107", password: "pw4apj", password_confirmation: "pw4apj" },
+           {first_name: "Jim", last_name: "Smith", phone_number: "9012680070", email: "mrsmithsemail@gmail.com", email_confirmed: true, address: "2001 New St", city: "Memphis", state: "TN", zip: "38111", password: "MrSmithsemail", password_confirmation: "MrSmithsemail" }]
+
+parents.each do |parent|
+  Parent.create(parent)
+end
+
+puts "There are now #{Parent.count} parents in the database"
