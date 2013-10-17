886320f60264a379642e84b1142c06a8c5991f7f
Koos755 <justinusadriaanse@Justinuss-MacBook-Pro.local>
2013-10-17 20:23:14 +0200

    added sessions controller

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -76,6 +76,6 @@ class ParentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def parent_params
-      params.require(:parent).permit(:first_name, :last_name, :phone_number, :email, :email_confirmed, :address, :city, :state, :zip, :password, :password_confirmation)
+      params.require(:parent).permit(:first_name, :last_name, :phone_number, :email, :email_confirmed, :address, :city, :state, :zip, :password, :password_confirmation, :terms)
     end
 end
