ee122b1ca0fd4f88241ac3f0f1a15da3a2e7b14b
Koos755 <justinusadriaanse@Justinuss-MacBook-Pro.local>
2013-10-17 20:43:54 +0200

    added methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    parent = Parent.find_by(params[:email])
+    if  parent.present? && parent.authenticate(params[:password])
+      session[:parent_id] = parent.id
+      redirect_to parent_url(parent)
+    else
+      flash.now[:error] = "Something went wrong, please try again."
+      render 'new'
+    end
   end
 
   def destroy
-
+    reset_session
+    redirect_to root_url, notice: "Successfully signed out"
   end
 
 end
