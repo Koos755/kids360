901ce4b83366fdaeedd6d03069493bebf8a66f85
Koos755 <justinusadriaanse@Justinuss-MacBook-Pro.local>
2013-10-22 22:48:45 +0200

    To address for contact for changed
     to team@kids360now.com

--- app/mailers/parent_mailer.rb
+++ app/mailers/parent_mailer.rb
@@ -22,6 +22,6 @@ class ParentMailer < ActionMailer::Base
     @name = name
     @email = email
     @message = message
-    mail(from: 'contact@kids360now.com', to: 'jadriaanse@me.com', subject: "Contact form from Kids360")
+    mail(from: 'contact@kids360now.com', to: 'team@kids360now.com', subject: "Contact form from Kids360")
   end
 end
