4e923f450dc1e565d25fd9a9261e2f693dea46ba
Koos755 <justinusadriaanse@Justinuss-MacBook-Pro.local>
2013-10-22 16:25:21 +0200

    trying on heroku again

--- app/models/token.rb
+++ app/models/token.rb
@@ -7,7 +7,7 @@ class Token < ActiveRecord::Base
   validates :parent_id, presence: true
   validates :token_type, presence: true, inclusion: { in: %w(confirmation password_reset), message: "%{value} is not a valid token_type" }
 
-  # after_save :send_email
+  after_save :send_email
 
   def create_confirmation_token(parent)
     self.value = SecureRandom.urlsafe_base64(30)
