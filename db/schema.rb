7d44dc95fcdb0604ca43785b9d7a6b3072f5f309
Justinus Adriaanse <jadriaanse@me.com>
2013-10-31 10:23:45 +0200

    added admin field to parents

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131023083038) do
+ActiveRecord::Schema.define(version: 20131031082306) do
 
   create_table "authorizations", force: true do |t|
     t.integer  "organization_id"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20131023083038) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "terms"
+    t.boolean  "admin"
   end
 
   create_table "tokens", force: true do |t|
