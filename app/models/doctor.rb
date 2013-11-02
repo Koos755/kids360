c252c9e74e4658e52403dc054b9571c70663c0b0
Justinus Adriaanse <jadriaanse@me.com>
2013-11-02 07:49:37 +0200

    added before delete funtion for doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,4 +4,13 @@ class Doctor < ActiveRecord::Base
    validates :name, presence: true
    validates :phone, presence: true
 
+   before_destroy :delete_child_foreign_key
+
+   def delete_child_foreign_key
+     children = Child.where(doctor_id: self.id)
+     children.each do |child|
+        child.doctor_id = nil
+        child.save
+     end
+   end
 end
