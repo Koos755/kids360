3aea5e8ff4ab4ebfe91040b209faae6dd92975ae
Justinus Adriaanse <jadriaanse@me.com>
2013-11-02 07:56:36 +0200

    Added Doctors to Models

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,11 +1,12 @@
 class Dashboard
 
-  attr_reader :parents_count, :children_count, :approved_organizations, :suggested_organizations
+  attr_reader :parents_count, :children_count, :approved_organizations, :suggested_organizations, :doctors_count
 
   def initialize
     @parents_count = Parent.count
     @children_count = Child.count
     @approved_organizations = Organization.where(active: true).count
+    @doctors_count = Doctor.count
     @suggested_organizations = Organization.where(active: nil)
   end
 
